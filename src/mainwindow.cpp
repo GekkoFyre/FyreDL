3a0088f3742d6c561d654aa69ac44d7880d30d16
Phobos D'thorga <phobos.gekko@gmail.com>
2016-09-20 16:03:09 +1000

    * Minor changes to the main interface.
    Signed-off-by: Phobos D'thorga <phobos.gekko@gmail.com>

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -13,3 +13,18 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_action_Open_a_File_triggered()
+{}
+
+void MainWindow::on_actionEnter_URL_triggered()
+{}
+
+void MainWindow::on_actionE_xit_triggered()
+{}
+
+void MainWindow::on_action_About_triggered()
+{}
+
+void MainWindow::on_action_Documentation_triggered()
+{}
--- src/mainwindow.hpp
+++ src/mainwindow.hpp
@@ -15,6 +15,17 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_action_Open_a_File_triggered();
+
+    void on_actionEnter_URL_triggered();
+
+    void on_actionE_xit_triggered();
+
+    void on_action_About_triggered();
+
+    void on_action_Documentation_triggered();
+
 private:
     Ui::MainWindow *ui;
 };
